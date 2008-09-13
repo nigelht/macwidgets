b476917c9d1421817d31f6c1c1e14146d8d4707f
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2008-09-13 10:59:31 +0000

    Updated comment.

--- trunk/source/com/explodingpixels/macwidgets/BottomBarSize.java
+++ trunk/source/com/explodingpixels/macwidgets/BottomBarSize.java
@@ -3,7 +3,7 @@ package com.explodingpixels.macwidgets;
 /**
  * An enumeration encapsulating the possible sizes of a Bottom Bar. The sizes called out by Apple in
  * Human Interface Guideline <a href="http://developer.apple.com/documentation/UserExperience/Conceptual/AppleHIGuidelines/XHIGWindows/chapter_18_section_4.html#//apple_ref/doc/uid/20000961-SW6">here</a>
- * are a 22 pixel and 32 pixel area. These correspond to the {@link #SMALL} and {@link #LARGE}
+ * are 22 pixel and 32 pixel areas. These correspond to the {@link #SMALL} and {@link #LARGE}
  * sizes. The {@link #EXTRA_SMALL} size isn't officially in the HIG, but can be seen in use in
  * Safari's status bar for example.
  */
