7a18fe8584f8f5c4488d954b2b0dd1830966b4ac
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2008-09-14 12:37:03 +0000

    Cleaned up formatting.

--- trunk/source/com/explodingpixels/macwidgets/MacColorUtils.java
+++ trunk/source/com/explodingpixels/macwidgets/MacColorUtils.java
@@ -10,61 +10,44 @@ public class MacColorUtils {
 
     public static Color LEOPARD_BORDER_COLOR = new Color(0x555555);
 
-    public static Color SOURCE_LIST_FOCUSED_BACKGROUND_COLOR =
-            new Color(0xd6dde5);
+    public static Color SOURCE_LIST_FOCUSED_BACKGROUND_COLOR = new Color(0xd6dde5);
 
-    public static Color SOURCE_LIST_UNFOCUSED_BACKGROUND_COLOR =
-            new Color(0xe8e8e8);
+    public static Color SOURCE_LIST_UNFOCUSED_BACKGROUND_COLOR = new Color(0xe8e8e8);
 
     // Leopard list colors. ///////////////////////////////////////////////////////////////////////
 
-    public static final Color MAC_FOCUSED_SOURCE_LIST_BACKGROUND_COLOR =
-            new Color(0xd6dde5);
+    public static final Color MAC_FOCUSED_SOURCE_LIST_BACKGROUND_COLOR = new Color(0xd6dde5);
 
-    public static final Color MAC_SOURCE_LIST_BACKGROUND_COLOR =
-            new Color(0xe8e8e8);
+    public static final Color MAC_SOURCE_LIST_BACKGROUND_COLOR = new Color(0xe8e8e8);
 
-    public static final Color MAC_SOURCE_LIST_CATEGORY_FONT_COLOR =
-            new Color(0x606e80);
+    public static final Color MAC_SOURCE_LIST_CATEGORY_FONT_COLOR = new Color(0x606e80);
 
-    public static final Color MAC_SOURCE_LIST_CATEGORY_FONT_SHADOW_COLOR =
-            new Color(255, 255, 255, 110);
+    public static final Color MAC_SOURCE_LIST_CATEGORY_FONT_SHADOW_COLOR = new Color(255, 255, 255, 110);
 
-    public static final Color MAC_SOURCE_LIST_ITEM_FONT_COLOR =
-            Color.BLACK;
+    public static final Color MAC_SOURCE_LIST_ITEM_FONT_COLOR = Color.BLACK;
 
-    public static final Color MAC_SOURCE_LIST_ITEM_FONT_SHADOW_COLOR =
-            new Color(0, 0, 0, 0);
+    public static final Color MAC_SOURCE_LIST_ITEM_FONT_SHADOW_COLOR = new Color(0, 0, 0, 0);
 
-    public static final Color MAC_SOURCE_LIST_SELECTED_ITEM_FONT_COLOR =
-            Color.WHITE;
+    public static final Color MAC_SOURCE_LIST_SELECTED_ITEM_FONT_COLOR = Color.WHITE;
 
-    public static final Color MAC_SOURCE_LIST_SELECTED_ITEM_FONT_SHADOW_COLOR =
-            new Color(0, 0, 0, 110);
+    public static final Color MAC_SOURCE_LIST_SELECTED_ITEM_FONT_SHADOW_COLOR = new Color(0, 0, 0, 110);
 
     // OS X unified toolbar colors. ///////////////////////////////////////////////////////////////
 
-    public static final Color OS_X_UNIFIED_TOOLBAR_FOCUSED_BOTTOM_COLOR =
-            new Color(64, 64, 64);
+    public static final Color OS_X_UNIFIED_TOOLBAR_FOCUSED_BOTTOM_COLOR = new Color(64, 64, 64);
 
-    public static final Color OS_X_UNIFIED_TOOLBAR_UNFOCUSED_BORDER_COLOR =
-            new Color(135, 135, 135);
+    public static final Color OS_X_UNIFIED_TOOLBAR_UNFOCUSED_BORDER_COLOR = new Color(135, 135, 135);
 
     // OS X bottom bar colors. ////////////////////////////////////////////////////////////////////
 
-    public static final Color OS_X_BOTTOM_BAR_ACTIVE_TOP_COLOR =
-            new Color(0xbbbbbb);
+    public static final Color OS_X_BOTTOM_BAR_ACTIVE_TOP_COLOR = new Color(0xbbbbbb);
 
-    public static final Color OS_X_BOTTOM_BAR_ACTIVE_BOTTOM_COLOR =
-            new Color(0x969696);
+    public static final Color OS_X_BOTTOM_BAR_ACTIVE_BOTTOM_COLOR = new Color(0x969696);
 
-    public static final Color OS_X_BOTTOM_BAR_INACTIVE_TOP_COLOR =
-            new Color(0xe3e3e3);
+    public static final Color OS_X_BOTTOM_BAR_INACTIVE_TOP_COLOR = new Color(0xe3e3e3);
 
-    public static final Color OS_X_BOTTOM_BAR_INACTIVE_BOTTOM_COLOR =
-            new Color(0xcfcfcf);
+    public static final Color OS_X_BOTTOM_BAR_INACTIVE_BOTTOM_COLOR = new Color(0xcfcfcf);
 
-    public static final Color OS_X_BOTTOM_BAR_BORDER_HIGHLIGHT_COLOR =
-            new Color(255, 255, 255, 110);
+    public static final Color OS_X_BOTTOM_BAR_BORDER_HIGHLIGHT_COLOR = new Color(255, 255, 255, 110);
 
 }
