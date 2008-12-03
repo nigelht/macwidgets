f524ecb01ea2f0e9b21a541fb90be8574822c874
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2008-12-03 12:24:12 +0000

    Changed method checking for Java 1.5.0_16 on Mac to check for Java 6 instead, and changed that methods name.

--- source/com/explodingpixels/util/PlatformUtils.java
+++ source/com/explodingpixels/util/PlatformUtils.java
@@ -21,17 +21,17 @@ public class PlatformUtils {
      * @return true if this JVM is running on a Mac.
      */
     public static boolean isMac() {
-        System.out.println(System.getProperty("os.name"));
         return System.getProperty("os.name").startsWith("Mac OS");
     }
 
     /**
-     * True if this JVM is running on a Mac and is Java for Mac OS X Update 2 (1.5.0_16).
+     * True if this JVM is running Java 6 on a Mac.
      *
-     * @return true if this JVM is running on a Mac and is Java for Mac OS X Update 2 (1.5.0_16).
+     * @return true if this JVM is running Java 6 on a Mac.
      */
-    public static boolean isMacJavaUpdate2() {
-        return isMac() && getJavaVersion().equals("1.5.0_16");
+    public static boolean isJava6OnMac() {
+        System.out.println(getJavaVersion());
+        return isMac() && getJavaVersion().startsWith("1.6");
     }
 
 }
