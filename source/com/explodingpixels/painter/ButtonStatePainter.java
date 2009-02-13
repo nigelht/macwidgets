32f33fd8d2df08121254c79ada499cee0d9932b9
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2009-02-13 12:34:06 +0000

    Updated Javadoc.

--- source/com/explodingpixels/painter/ButtonStatePainter.java
+++ source/com/explodingpixels/painter/ButtonStatePainter.java
@@ -1,9 +1,16 @@
 package com.explodingpixels.painter;
 
-import javax.swing.AbstractButton;
-import java.awt.Component;
-import java.awt.Graphics2D;
+import javax.swing.*;
+import java.awt.*;
 
+/**
+ * A {@link Painter} that can be used to paint the various states of a button. This painter will delegate to the
+ * supplied painters based on the current state of the button. That is, if the button is being "rolled over", the
+ * {@code rolloverPainter} will be called; if the button is pressed, then the {@code pressedPainter} will be called,
+ * etc.
+ *
+ * @param <B>
+ */
 public class ButtonStatePainter<B extends AbstractButton>
         implements Painter<B> {
 
@@ -12,10 +19,28 @@ public class ButtonStatePainter<B extends AbstractButton>
     private final Painter<Component> fPressedPainter;
     private final Painter<Component> fSelectedPainter;
 
+    /**
+     * Creates a painter that will always use the given {@link Painter} to
+     * paint the button.
+     * @param defaultPainter the {@code Painter} to use to paint the
+     *        button.
+     */
     public ButtonStatePainter(Painter<Component> defaultPainter) {
         this(defaultPainter, defaultPainter, defaultPainter, defaultPainter);
     }
 
+    /**
+     * Creates a painter that will delegate to the given painters based on
+     * the current state of the button.
+     * @param defaultPainter the {@link Painter} to use when the button has
+     *        no specific state.
+     * @param rolloverPainter the {@code Painter} to use when the button is
+     *        being "rolled over".
+     * @param pressedPainter the {@code Painter} to use when the button is
+     *        being pressed.
+     * @param selectedPainter the {@code Painter} to use when the button has
+     *        been selected.
+     */
     public ButtonStatePainter(Painter<Component> defaultPainter,
                               Painter<Component> rolloverPainter,
                               Painter<Component> pressedPainter,
@@ -40,10 +65,11 @@ public class ButtonStatePainter<B extends AbstractButton>
 
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // Dummy Painter implementation.
-    ///////////////////////////////////////////////////////////////////////////
+    // Dummy Painter implementation. /////////////////////////////////////////////////
 
+    /**
+     * An implementation of {@link Painter} that does no painting.
+     */
     public static class DummyPainter implements Painter<Component> {
         public void paint(Graphics2D g, Component component, int width, int height) {
             // do nothing.
