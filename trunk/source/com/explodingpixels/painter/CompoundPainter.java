6820e18eca12a0d469efe37da9a2fffe3bab0fff
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2009-02-13 12:57:37 +0000

    Updated Javadoc.

--- trunk/source/com/explodingpixels/painter/CompoundPainter.java
+++ trunk/source/com/explodingpixels/painter/CompoundPainter.java
@@ -1,11 +1,20 @@
 package com.explodingpixels.painter;
 
-import java.awt.Graphics2D;
+import java.awt.*;
 
+/**
+ * An implementation of {@link Painter} that calls a series of {@code Painter}s in succession. {@code ComponentPainter}
+ * does not do any painting itself.
+ */
 public class CompoundPainter<T> implements Painter<T> {
 
     private Painter<T>[] fPainters;
 
+    /**
+     * Creates a {@link Painter} that calls the given {@code Painter}s in the order they are supplied when
+     * {@link #paint(java.awt.Graphics2D, Object, int, int)} is called.
+     * @param painters the {@code Painter}s to delegate to.
+     */
     public CompoundPainter(Painter<T>... painters) {
         fPainters = painters;
     }
