8dd93663f14463c51aa333db2fc69c193af6d692
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2009-06-19 00:04:46 +0000

    Intermediate checkin.

--- trunk/demo/com/explodingpixels/macwidgets/DITunesTableUI.java
+++ trunk/demo/com/explodingpixels/macwidgets/DITunesTableUI.java
@@ -18,30 +18,34 @@ public class DITunesTableUI {
             public void run() {
 
                 String[][] data = new String[][]{
-                        {"Batman Begins", "2005"}, {"The Dark Knight", "2008"},
-                        {"Austin Powers", "1997"}, {"The Last Samurai", "2003"},
-                        {"Back to the Future", "1985"},
-                        {"Batman Begins", "2005"}, {"The Dark Knight", "2008"},
-                        {"Austin Powers", "1997"}, {"The Last Samurai", "2003"},
-                        {"Back to the Future", "1985"},
-                        {"Batman Begins", "2005"}, {"The Dark Knight", "2008"},
-                        {"Austin Powers", "1997"}, {"The Last Samurai", "2003"},
-                        {"Back to the Future", "1985"}
+                        {"All These Things I Hate (Revolve Around Me)", "Bullet For My Valentine", "The Poison"},
+                        {"Cries In Vain", "Bullet For My Valentine", "The Poison"},
+                        {"The End", "Bullet For My Valentine", "The Poison"},
+                        {"Her Voice Resides", "Bullet For My Valentine", "The Poison"},
+                        {"Hit The Floor", "Bullet For My Valentine", "The Poison"},
+                        {"Intro", "Bullet For My Valentine Apocalyptica", "The Poison"},
+                        {"The Poison", "Bullet For My Valentine", "The Poison"},
+                        {"Room 409", "Bullet For My Valentine", "The Poison"},
+                        {"Spit You Out", "Bullet For My Valentine", "The Poison"},
+                        {"Suffocating Under Words Of Sorrow (What Can I Do)", "Bullet For My Valentine", "The Poison"},
+                        {"Tears Don't Fall", "Bullet For My Valentine", "The Poison"},
+                        {"4 Words (To Choke Upon)", "Bullet For My Valentine", "The Poison"},
+                        {"10 Years Today", "Bullet For My Valentine", "The Poison"}
                 };
 
-                JTable table = new JTable(new DefaultTableModel(data, new String[]{"Movie Title", "Year Released"}));
+
+                JTable table = new JTable(new DefaultTableModel(data, new String[]{"Name", "Artist", "Album"}));
                 table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
                 table.setUI(new ITunesTableUI());
-//                table.setEnabled(false);
+                table.getColumnModel().getColumn(0).setPreferredWidth(150);
+                table.getColumnModel().getColumn(1).setPreferredWidth(100);
 
                 JScrollPane scrollPane = new JScrollPane(table);
                 scrollPane.setBorder(BorderFactory.createEmptyBorder());
 
                 JFrame frame = new JFrame();
                 frame.add(scrollPane, BorderLayout.CENTER);
-//                frame.getRootPane().add(new JLabel("hello"));
-//                WindowUtils.createAndInstallRepaintWindowFocusListener(frame);
-                frame.setSize(300, 225);
+                frame.setSize(400, 265);
                 frame.setLocationRelativeTo(null);
                 frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 frame.setVisible(true);
--- trunk/source/com/explodingpixels/macwidgets/plaf/ITunesTableUI.java
+++ trunk/source/com/explodingpixels/macwidgets/plaf/ITunesTableUI.java
@@ -7,24 +7,31 @@ import com.explodingpixels.macwidgets.MacFontUtils;
 import com.explodingpixels.widgets.TableUtils;
 import com.explodingpixels.widgets.WindowUtils;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JComponent;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicTableUI;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
 
+/**
+ * A UI delegate that renders an iTunes style table.
+ * <br/>
+ * <img src="../../../../../graphics/iTunesTable.png">
+ */
 public class ITunesTableUI extends BasicTableUI {
 
     private static final Color EVEN_ROW_COLOR = new Color(241, 245, 250);
     private static final Color TABLE_GRID_COLOR = new Color(0xd9d9d9);
-    private static final Color SCROLLPANE_BORDER_COLOR = new Color(0x555555);
     private static final Color SELECTION_ACTIVE_SELECTION_FOREGROUND_COLOR = Color.WHITE;
     private static final Color SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0x3d80df);
     private static final Color SELECTION_INACTIVE_SELECTION_FOREGROUND_COLOR = Color.BLACK;
@@ -52,6 +59,8 @@ public class ITunesTableUI extends BasicTableUI {
 
         table.setDefaultRenderer(Rating.class, new ITunesRatingTableCellRenderer());
         table.setDefaultRenderer(Object.class, createTransparentTableCellRenderer());
+
+        table.setDefaultEditor(Object.class, createDefaultTableCellEditor());
 
         makeTableActive();
         WindowUtils.installWindowFocusListener(createWindowFocusListener(), table);
@@ -82,11 +91,23 @@ public class ITunesTableUI extends BasicTableUI {
             @Override
             public Component getTableCellRendererComponent(
                     JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-                Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-                ((JComponent) c).setOpaque(isSelected);
-                return c;
+                Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (component instanceof JComponent) {
+                    JComponent jcomponent = (JComponent) component;
+                    jcomponent.setOpaque(isSelected);
+                    jcomponent.setBorder(table.isRowSelected(row)
+                            ? getSelectedRowBorder() : getRowBorder());
+                }
+                return component;
             }
         };
+    }
+
+    private TableCellEditor createDefaultTableCellEditor() {
+        JTextField textField = new JTextField();
+        textField.setFont(MacFontUtils.ITUNES_FONT);
+        textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        return new DefaultCellEditor(textField);
     }
 
     private WindowFocusListener createWindowFocusListener() {
@@ -101,24 +122,18 @@ public class ITunesTableUI extends BasicTableUI {
         };
     }
 
-    @Override
-    public void paint(Graphics g, JComponent c) {
-        super.paint(g, c);
-        // TODO draw separators between selected cells.
-        paintSelectedRowHighlights(g);
+    private Border getRowBorder() {
+        return BorderFactory.createEmptyBorder(0, 5, 0, 5);
     }
 
-    private void paintSelectedRowHighlights(Graphics g) {
-        for (int row : table.getSelectedRows()) {
-            Rectangle clip = g.getClipBounds();
-            int y = ((row + 1) * table.getRowHeight()) - 1;
-            g.setColor(getSelectedRowBottomHighlight());
-            g.drawLine(clip.x, y, clip.x + clip.width, y);
-        }
+    private Border getSelectedRowBorder() {
+        return BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 1, 0, getSelectedRowBottomHighlight()),
+                BorderFactory.createEmptyBorder(1, 5, 0, 5));
     }
 
     private Color getSelectedRowBottomHighlight() {
-        return table.hasFocus()
+        return WindowUtils.isParentWindowFocused(table)
                 ? SELECTION_ACTIVE_BOTTOM_BORDER_COLOR : SELECTION_INACTIVE_BOTTOM_BORDER_COLOR;
     }
 
