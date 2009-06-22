137490069e11959ce33869f58d3975bb28c632ee
kenneth.orr <kenneth.orr@994baef0-d455-0410-a6e6-f5fefbad0d02>
2009-06-22 11:59:48 +0000

    Intermediate checkin.

--- source/com/explodingpixels/widgets/TableHeaderUtils.java
+++ source/com/explodingpixels/widgets/TableHeaderUtils.java
@@ -1,24 +1,17 @@
 package com.explodingpixels.widgets;
 
-import javax.swing.CellRendererPane;
-import javax.swing.JComponent;
-import javax.swing.JTable;
-import javax.swing.JViewport;
+import javax.swing.*;
 import javax.swing.border.AbstractBorder;
 import javax.swing.border.Border;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumnModel;
-import java.awt.Component;
-import java.awt.Cursor;
-import java.awt.Graphics;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.*;
 
 public class TableHeaderUtils {
 
     private static final CellRendererPane CELL_RENDER_PANE = new CellRendererPane();
+
+    public static final int NO_COLUMN = -1;
 
     private static final String PRESSED_COLUMN_KEY = "EPJTableHeader.pressedColumn";
     private static final String SELECTED_COLUMN_KEY = "EPJTableHeader.selectedColumn";
@@ -28,11 +21,11 @@ public class TableHeaderUtils {
         // no constructor - utility class.
     }
 
-    // Support for making a component look like a given tables default table header ///////////////
-
     /**
-     * Creates a component that paints the header background, which can be used, for example, in a
-     * {@link javax.swing.JScrollPane} corner.
+     * Creates a component that paints the table's header background.
+     *
+     * @param table the {@link JTable} to create the corner component for.
+     * @return a {@link JComponent} that paints the given table's table header background.
      */
     public static JComponent createCornerComponent(final JTable table) {
         return new JComponent() {
@@ -43,8 +36,13 @@ public class TableHeaderUtils {
         };
     }
 
-    // Support for making a table header fill the empty space. ////////////////////////////////////
-
+    /**
+     * Installs a custom {@link Border} on the given table's {@link JTableHeader} that paints any
+     * blank area to the right of the last column header with the {@code JTableHeader}'s background.
+     *
+     * @param table the {@link JTable} from which to get the {@code JTableHeader} to paint the
+     *              empty column header space for.
+     */
     public static void makeHeaderFillEmptySpace(JTable table) {
         table.getTableHeader().setBorder(createTableHeaderEmptyColumnPainter(table));
     }
@@ -65,6 +63,10 @@ public class TableHeaderUtils {
                 width, table.getTableHeader().getHeight(), true);
     }
 
+    /**
+     * Creates a {@link Border} that paints any empty space to the right of the last column header
+     * in the given {@link JTable}'s {@link JTableHeader}.
+     */
     private static Border createTableHeaderEmptyColumnPainter(final JTable table) {
         return new AbstractBorder() {
             @Override
@@ -83,32 +85,92 @@ public class TableHeaderUtils {
 
     // Support for making column headers selected and sorted. /////////////////////////////////////
 
+    /**
+     * Get's the pressed column header for the given {@link JTableHeader}.
+     *
+     * @param tableHeader the {@code JTableHeader} to determine the pressed column header for.
+     * @return the column model index of the pressed column header, or {@link #NO_COLUMN} if no
+     *         column's header has been pressed.
+     * @see #isColumnPressed(javax.swing.table.JTableHeader, int)
+     */
     public static int getPressedColumn(JTableHeader tableHeader) {
         Object pressedColumnValue = tableHeader.getClientProperty(PRESSED_COLUMN_KEY);
         return pressedColumnValue != null && pressedColumnValue instanceof Integer
-                ? ((Integer) pressedColumnValue) : -1;
+                ? ((Integer) pressedColumnValue) : NO_COLUMN;
     }
 
+    /**
+     * {@code true} if the given column model index is pressed in the given {@link JTableHeader}.
+     *
+     * @param tableHeader      the {@code JTableHeader} to use when determining if the given column is
+     *                         pressed.
+     * @param columnModelIndex the column model index to determine the pressed state of.
+     * @return {@code true} if the given column is pressed.
+     * @see #getPressedColumn(javax.swing.table.JTableHeader)
+     * @see #setPressedColumn(javax.swing.table.JTableHeader, int)
+     */
     public static boolean isColumnPressed(JTableHeader tableHeader, int columnModelIndex) {
         int pressedColumn = getPressedColumn(tableHeader);
         return pressedColumn >= 0 && pressedColumn == columnModelIndex;
     }
 
+    /**
+     * Sets the given column for the given {@link JTableHeader} as pressed. Calling this method
+     * installs a hint, which can be used by renders to draw the correct column pressed state.
+     * Renders can use {@link #isColumnPressed(javax.swing.table.JTableHeader, int)} to determine
+     * what state to draw the header for the given index in.
+     * header state.
+     *
+     * @param tableHeader      the {@code JTableHeader} to set the pressed state for.
+     * @param columnModelIndex the column model index of the pressed column.
+     */
     public static void setPressedColumn(JTableHeader tableHeader, int columnModelIndex) {
         tableHeader.putClientProperty(PRESSED_COLUMN_KEY, columnModelIndex);
     }
 
+    /**
+     * Get's the selected column header for the given {@link JTableHeader}.
+     *
+     * @param tableHeader the {@code JTableHeader} to determine the selected column header for.
+     * @return the column model index of the selected column header, or {@link #NO_COLUMN} if no
+     *         column's header has been pressed.
+     * @see #isColumnSelected(javax.swing.table.JTableHeader, int)
+     */
     public static int getSelectedColumn(JTableHeader tableHeader) {
         Object selectedColumnValue = tableHeader.getClientProperty(SELECTED_COLUMN_KEY);
         return selectedColumnValue != null && selectedColumnValue instanceof Integer
-                ? ((Integer) selectedColumnValue) : -1;
+                ? ((Integer) selectedColumnValue) : NO_COLUMN;
     }
 
+    /**
+     * {@code true} if the given column model index is selected in the given {@link JTableHeader}.
+     * <p/>
+     * Note that there is no direct way to set wheter a column is selected or not. Selection is a
+     * by-product of set the sort direction on a column.
+     *
+     * @param tableHeader      the {@code JTableHeader} to use when determining if the given column is
+     *                         selected.
+     * @param columnModelIndex the column model index to determine the selected state of.
+     * @return {@code true} if the given column is selected.
+     * @see #getSelectedColumn(javax.swing.table.JTableHeader)
+     * @see #setSortDirection(javax.swing.table.JTableHeader, int, com.explodingpixels.widgets.TableUtils.SortDirection)
+     */
     public static boolean isColumnSelected(JTableHeader tableHeader, int columnModelIndex) {
         int selectedColumn = getSelectedColumn(tableHeader);
         return selectedColumn >= 0 && selectedColumn == columnModelIndex;
     }
 
+    /**
+     * Get's the pressed column header for the given {@link JTableHeader}.
+     *
+     * @param tableHeader      the {@code JTableHeader} containing the column to determine the sort
+     *                         direction for.
+     * @param columnModelIndex the column model index to determine the sort direction for.
+     * @return the {@link com.explodingpixels.widgets.TableUtils.SortDirection} of the given
+     *         column index.
+     * @see #setSortDirection(javax.swing.table.JTableHeader, int, com.explodingpixels.widgets.TableUtils.SortDirection)
+     * @see #toggleSortDirection(javax.swing.table.JTableHeader, int)
+     */
     public static TableUtils.SortDirection getSortDirection(JTableHeader tableHeader, int columnModelIndex) {
         Object sortDirection = tableHeader.getClientProperty(SORT_DIRECTION_KEY);
         boolean isColumnSelected = isColumnSelected(tableHeader, columnModelIndex);
@@ -116,16 +178,35 @@ public class TableHeaderUtils {
                 ? TableUtils.SortDirection.find((String) sortDirection) : TableUtils.SortDirection.NONE;
     }
 
+    /**
+     * Toggles the sort direction of the given column model index in the given {@link JTableHeader}.
+     * This also makes the given column selected. If the given column has no sort order set
+     * ({@link com.explodingpixels.widgets.TableUtils.SortDirection#NONE}) then the new sort order
+     * will be {@link com.explodingpixels.widgets.TableUtils.SortDirection#ASCENDING}. Otherwise
+     * the new sort order will be {@link com.explodingpixels.widgets.TableUtils.SortDirection#DESCENDING}
+     *
+     * @param tableHeader      the {@code JTableHeader} of the column to toggle the sort order for.
+     * @param columnModelIndex the column model index to toggle the sort order for.
+     * @return the new sort order of the column.
+     */
     public static TableUtils.SortDirection toggleSortDirection(JTableHeader tableHeader, int columnModelIndex) {
-        Object oldSortDirection = tableHeader.getClientProperty(SORT_DIRECTION_KEY);
+        // grab the previously selected column as well as the sort direction for the given column, if either exist.
+        int oldSelectedColumn = getSelectedColumn(tableHeader);
+        TableUtils.SortDirection oldSortDirection = getSortDirection(tableHeader, columnModelIndex);
+
+        // if the old selected column is different from the given column, or the old sort direction is set to none,
+        //    then the new sort direction is "ascending".
+        // else the new sort direction is "descending.
         TableUtils.SortDirection newSortDirection;
-        if (oldSortDirection == null) {
+        if (oldSelectedColumn != columnModelIndex || oldSortDirection == TableUtils.SortDirection.NONE) {
             newSortDirection = TableUtils.SortDirection.ASCENDING;
         } else {
             newSortDirection =
-                    TableUtils.SortDirection.find((String) oldSortDirection) == TableUtils.SortDirection.ASCENDING
+                    oldSortDirection == TableUtils.SortDirection.ASCENDING
                             ? TableUtils.SortDirection.DESCENDING : TableUtils.SortDirection.ASCENDING;
         }
+
+        // set the new sort direction on the given column model index.
         setSortDirection(tableHeader, columnModelIndex, newSortDirection);
 
         return newSortDirection;
@@ -135,75 +216,6 @@ public class TableHeaderUtils {
                                          TableUtils.SortDirection sortDirection) {
         tableHeader.putClientProperty(SELECTED_COLUMN_KEY, columnModelIndex);
         tableHeader.putClientProperty(SORT_DIRECTION_KEY, sortDirection.getValue());
-    }
-
-    // Support for listening to header clicks. ////////////////////////////////////////////////////
-
-    public interface ColumnHeaderClickListener {
-        /**
-         * Called when a column header in an associated {@link JTable} is pressed.
-         *
-         * @param columnModelIndex the model index of the column header that was pressed.
-         */
-        void columnHeaderPressed(int columnModelIndex);
-
-        /**
-         * Called when a column header in an associated {@link JTable} is clicked.
-         *
-         * @param columnModelIndex the model index of the column header that was clicked.
-         */
-        void columnHeaderClicked(int columnModelIndex);
-
-        /**
-         * Called when the column header that was pressed has been released.
-         */
-        void columnHeaderReleased();
-    }
-
-    public static MouseListener createColumnHeaderSortHandler(
-            final JTable table, final TableUtils.SortDelegate sortDelegate) {
-        return new MouseAdapter() {
-            private boolean fMouseEventIsPerformingPopupTrigger = false;
-
-            public void mouseClicked(MouseEvent mouseEvent) {
-                if (shouldProcessMouseClicked()) {
-                    final TableColumnModel columnModel = table.getColumnModel();
-                    int columnViewIndex = columnModel.getColumnIndexAtX(mouseEvent.getX());
-                    int columnModelIndex = table.convertColumnIndexToModel(columnViewIndex);
-                    TableUtils.SortDirection sortDirection =
-                            TableHeaderUtils.toggleSortDirection(table.getTableHeader(), columnModelIndex);
-                    sortDelegate.sort(columnModelIndex, sortDirection);
-
-                    table.getTableHeader().repaint();
-                }
-            }
-
-            private boolean shouldProcessMouseClicked() {
-                return !fMouseEventIsPerformingPopupTrigger
-                        && table.getTableHeader().getCursor() != Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR);
-            }
-
-            public void mousePressed(MouseEvent mouseEvent) {
-                fMouseEventIsPerformingPopupTrigger = mouseEvent.isPopupTrigger();
-
-                if (table.getTableHeader().getCursor() != Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR)) {
-                    final TableColumnModel columnModel = table.getColumnModel();
-                    int viewColumnIndex = columnModel.getColumnIndexAtX(mouseEvent.getX());
-                    int columnModelIndex = table.convertColumnIndexToModel(viewColumnIndex);
-//                    listener.columnHeaderPressed(columnModelIndex);
-                    TableHeaderUtils.setPressedColumn(table.getTableHeader(), columnModelIndex);
-
-                    table.getTableHeader().repaint();
-                }
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-//                listener.columnHeaderReleased();
-                TableHeaderUtils.setPressedColumn(table.getTableHeader(), -1);
-                table.getTableHeader().repaint();
-            }
-        };
     }
 
 }
