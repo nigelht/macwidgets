ea8d8ef43206d13e95853735fe833dc6e438533a
dlemmermann <dlemmermann@994baef0-d455-0410-a6e6-f5fefbad0d02>
2010-08-27 14:25:38 +0000

    Fix for issue 106 "ITunesTableUI selection should span width of viewport" Fix for issue 167 "Alternating row colors in ITunesTable can appear messed up"

--- trunk/source/com/explodingpixels/macwidgets/plaf/ITunesTableUI.java
+++ trunk/source/com/explodingpixels/macwidgets/plaf/ITunesTableUI.java
@@ -41,115 +41,114 @@ import com.explodingpixels.widgets.WindowUtils;
 public class ITunesTableUI extends BasicTableUI {
 
 	protected static final Color EVEN_ROW_COLOR = new Color(241, 245, 250);
-    protected static final Color TABLE_GRID_COLOR = new Color(0xd9d9d9);
-    protected static final Color SELECTION_ACTIVE_SELECTION_FOREGROUND_COLOR = Color.WHITE;
-    protected static final Color SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0x3d80df);
-    protected static final Color SELECTION_INACTIVE_SELECTION_FOREGROUND_COLOR = Color.BLACK;
-    protected static final Color SELECTION_INACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0xc0c0c0);
-    protected static final Color SELECTION_ACTIVE_BOTTOM_BORDER_COLOR = new Color(125, 170, 234);
-    protected static final Color SELECTION_INACTIVE_BOTTOM_BORDER_COLOR = new Color(224, 224, 224);
-    protected static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
+	protected static final Color TABLE_GRID_COLOR = new Color(0xd9d9d9);
+	protected static final Color SELECTION_ACTIVE_SELECTION_FOREGROUND_COLOR = Color.WHITE;
+	protected static final Color SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0x3d80df);
+	protected static final Color SELECTION_INACTIVE_SELECTION_FOREGROUND_COLOR = Color.BLACK;
+	protected static final Color SELECTION_INACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0xc0c0c0);
+	protected static final Color SELECTION_ACTIVE_BOTTOM_BORDER_COLOR = new Color(125, 170, 234);
+	protected static final Color SELECTION_INACTIVE_BOTTOM_BORDER_COLOR = new Color(224, 224, 224);
+	protected static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
 
-    @Override
-    public void installUI(JComponent c) {
-        super.installUI(c);
+	@Override
+	public void installUI(JComponent c) {
+		super.installUI(c);
 
-        table.remove(rendererPane);
-        rendererPane = createCustomCellRendererPane();
-        table.add(rendererPane);
+		table.remove(rendererPane);
+		rendererPane = createCustomCellRendererPane();
+		table.add(rendererPane);
 
-        // TODO save defaults.
+		// TODO save defaults.
 
-        table.setOpaque(false);
-        table.setFont(MacFontUtils.ITUNES_FONT);
-        table.setGridColor(TABLE_GRID_COLOR);
-        table.setIntercellSpacing(new Dimension(0, 0));
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.setOpaque(false);
+		table.setFont(MacFontUtils.ITUNES_FONT);
+		table.setGridColor(TABLE_GRID_COLOR);
+		table.setIntercellSpacing(new Dimension(0, 0));
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-        table.getTableHeader().setDefaultRenderer(new ITunesTableHeaderRenderer(table));
+		table.getTableHeader().setDefaultRenderer(new ITunesTableHeaderRenderer(table));
 
-        table.setShowHorizontalLines(false);
-        TableHeaderUtils.makeHeaderFillEmptySpace(table);
-        TableUtils.makeStriped(table, EVEN_ROW_COLOR);
+		table.setShowHorizontalLines(false);
+		TableHeaderUtils.makeHeaderFillEmptySpace(table);
+		TableUtils.makeStriped(table, EVEN_ROW_COLOR);
 
-        table.setDefaultRenderer(Rating.class, new ITunesRatingTableCellRenderer());
-        table.setDefaultEditor(Object.class, createDefaultTableCellEditor());
+		table.setDefaultRenderer(Rating.class, new ITunesRatingTableCellRenderer());
+		table.setDefaultEditor(Object.class, createDefaultTableCellEditor());
 
-        makeTableActive();
-        WindowUtils.installWeakWindowFocusListener(table, createWindowFocusListener());
-    }
+		makeTableActive();
+		WindowUtils.installWeakWindowFocusListener(table, createWindowFocusListener());
+	}
 
-    @Override
-    protected void installListeners() {
-        super.installListeners();
-    }
+	@Override
+	protected void installListeners() {
+		super.installListeners();
+	}
 
-    private void makeTableActive() {
-        table.setSelectionForeground(SELECTION_ACTIVE_SELECTION_FOREGROUND_COLOR);
-        table.setSelectionBackground(SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR);
-    }
+	private void makeTableActive() {
+		table.setSelectionForeground(SELECTION_ACTIVE_SELECTION_FOREGROUND_COLOR);
+		table.setSelectionBackground(SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR);
+	}
 
-    private void makeTableInactive() {
-        table.setSelectionForeground(SELECTION_INACTIVE_SELECTION_FOREGROUND_COLOR);
-        table.setSelectionBackground(SELECTION_INACTIVE_SELECTION_BACKGROUND_COLOR);
-    }
+	private void makeTableInactive() {
+		table.setSelectionForeground(SELECTION_INACTIVE_SELECTION_FOREGROUND_COLOR);
+		table.setSelectionBackground(SELECTION_INACTIVE_SELECTION_BACKGROUND_COLOR);
+	}
 
-    private TableCellEditor createDefaultTableCellEditor() {
-        JTextField textField = new JTextField();
-        textField.setFont(MacFontUtils.ITUNES_FONT);
-        textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        return new DefaultCellEditor(textField);
-    }
+	private TableCellEditor createDefaultTableCellEditor() {
+		JTextField textField = new JTextField();
+		textField.setFont(MacFontUtils.ITUNES_FONT);
+		textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		return new DefaultCellEditor(textField);
+	}
 
-    private WindowFocusListener createWindowFocusListener() {
-        return new WindowFocusListener() {
-            public void windowGainedFocus(WindowEvent e) {
-                makeTableActive();
-            }
+	private WindowFocusListener createWindowFocusListener() {
+		return new WindowFocusListener() {
+			public void windowGainedFocus(WindowEvent e) {
+				makeTableActive();
+			}
 
-            public void windowLostFocus(WindowEvent e) {
-                makeTableInactive();
-            }
-        };
-    }
+			public void windowLostFocus(WindowEvent e) {
+				makeTableInactive();
+			}
+		};
+	}
 
-    protected Border getRowBorder() {
-        return BorderFactory.createEmptyBorder(0, 5, 0, 5);
-    }
+	public Border getRowBorder() {
+		return BorderFactory.createEmptyBorder(0, 5, 0, 5);
+	}
 
-    protected Border getSelectedRowBorder() {
-        return BorderFactory.createCompoundBorder(
-                BorderFactory.createMatteBorder(0, 0, 1, 0, getSelectedRowBottomHighlight()),
-                BorderFactory.createEmptyBorder(1, 5, 0, 5));
-    }
+	public Border getSelectedRowBorder() {
+		return BorderFactory.createCompoundBorder(
+				BorderFactory.createMatteBorder(0, 0, 1, 0, getSelectedRowBottomHighlight()),
+				BorderFactory.createEmptyBorder(1, 5, 0, 5));
+	}
 
-    private Color getSelectedRowBottomHighlight() {
-        return WindowUtils.isParentWindowFocused(table)
-                ? SELECTION_ACTIVE_BOTTOM_BORDER_COLOR : SELECTION_INACTIVE_BOTTOM_BORDER_COLOR;
-    }
+	private Color getSelectedRowBottomHighlight() {
+		return WindowUtils.isParentWindowFocused(table)
+		? SELECTION_ACTIVE_BOTTOM_BORDER_COLOR : SELECTION_INACTIVE_BOTTOM_BORDER_COLOR;
+	}
 
-    /**
-     * Creates a custom {@link CellRendererPane} that sets the renderer component to be non-opqaque
-     * if the associated row isn't selected. This custom {@code CellRendererPane} is needed because
-     * a table UI delegate has no prepare renderer like {@link JTable} has.
-     */
-    protected CellRendererPane createCustomCellRendererPane() {
-        return new CellRendererPane() {
-            @Override
-            public void paintComponent(Graphics graphics, Component component, Container container,
-                                       int x, int y, int w, int h, boolean shouldValidate) {
+	/**
+	 * Creates a custom {@link CellRendererPane} that sets the renderer component to be non-opqaque
+	 * if the associated row isn't selected. This custom {@code CellRendererPane} is needed because
+	 * a table UI delegate has no prepare renderer like {@link JTable} has.
+	 */
+	protected CellRendererPane createCustomCellRendererPane() {
+		return new CellRendererPane() {
+			@Override
+			public void paintComponent(Graphics graphics, Component component, Container container,
+					int x, int y, int w, int h, boolean shouldValidate) {
 
-                int rowAtPoint = table.rowAtPoint(new Point(x, y));
-                boolean isSelected = table.isRowSelected(rowAtPoint);
-                if (component instanceof JComponent) {
-                    JComponent jComponent = (JComponent) component;
-                    jComponent.setOpaque(isSelected);
-                    jComponent.setBorder(isSelected ? getSelectedRowBorder() : getRowBorder());
-                    jComponent.setBackground(isSelected ? jComponent.getBackground() : TRANSPARENT_COLOR);
-                }
-
-                super.paintComponent(graphics, component, container, x, y, w, h, shouldValidate);
-            }
-        };
-    }
+				int rowAtPoint = table.rowAtPoint(new Point(x, y));
+				boolean isSelected = table.isRowSelected(rowAtPoint);
+				if (component instanceof JComponent) {
+					JComponent jComponent = (JComponent) component;
+					jComponent.setOpaque(isSelected);
+					jComponent.setBorder(isSelected ? getSelectedRowBorder() : getRowBorder());
+					jComponent.setBackground(isSelected ? jComponent.getBackground() : TRANSPARENT_COLOR);
+				}
+				super.paintComponent(graphics, component, container, x, y, w, h, shouldValidate);
+			}
+		};
+	}
 }
