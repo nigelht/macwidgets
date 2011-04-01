fdeed832e77e7dbaf118786e3231c042c35e0cc9
ulrik.lejon@gmail.com <ulrik.lejon@gmail.com@994baef0-d455-0410-a6e6-f5fefbad0d02>
2011-04-01 11:39:40 +0000

    Added support for TreeWillExpandListener

--- trunk/demo/com/explodingpixels/macwidgets/DSourceListExpansionListener.java
+++ trunk/demo/com/explodingpixels/macwidgets/DSourceListExpansionListener.java
@@ -6,24 +6,60 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DSourceListExpansionListener {
+    private static Object[] willExpandOptions = {"Cancel Expansion", "Expand"};
+    private static String willExpandText = "A branch node is about to be expanded.\n"
+                                  + "Click \"Cancel Expansion\" to prevent it.";
+    private static String willExpandTitle = "Tree Will Expand";
 
     public static void main(String[] args) {
 
         final SourceListExpansionListener expansionListener = new SourceListExpansionListener() {
             public void sourceListItemExpanded(SourceListItem item) {
-                System.out.println(item.getText() + " was expanded.");
+                System.out.println("SourceListItem " +item.getText() + " was expanded.");
             }
 
             public void sourceListItemCollapsed(SourceListItem item) {
-                System.out.println(item.getText() + " was collapsed.");
+                System.out.println("SourceListItem " +item.getText() + " was collapsed.");
             }
 
             public void sourceListCategoryExpanded(SourceListCategory category) {
-                System.out.println(category.getText() + " was expanded.");
+                System.out.println("SourceListCategory " +category.getText() + " was expanded.");
             }
 
             public void sourceListCategoryCollapsed(SourceListCategory category) {
-                System.out.println(category.getText() + " was collapsed.");
+                System.out.println("SourceListCategory " +category.getText() + " was collapsed.");
+            }
+
+            public boolean shouldExpandSourceListItem(SourceListItem item) {
+                int n = JOptionPane.showOptionDialog(
+                        null, willExpandText, willExpandTitle,
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        willExpandOptions,
+                        willExpandOptions[1]);
+
+                if (n == JOptionPane.YES_OPTION) {
+                    return false;
+                }
+
+                System.out.println("SourceListItem " +item.getText() + " will be expanded.");
+                return true;
+            }
+
+            public boolean shouldCollapseSourceListItem(SourceListItem item) {
+                System.out.println("SourceListItem " +item.getText() + " will be collapsed.");
+                return true;
+            }
+
+            public boolean shouldExpandSourceListCategory(SourceListCategory category) {
+                System.out.println("SourceListCategory " +category.getText() + " will be expanded.");
+                return true;
+            }
+
+            public boolean shouldToCollapseSourceListCategory(SourceListCategory category) {
+                System.out.println("SourceListCategory " +category.getText() + " will be collapsed.");
+                return true;
             }
         };
 
