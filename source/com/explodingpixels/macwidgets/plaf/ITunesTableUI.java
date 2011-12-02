1e03fcdd9ec827b34eaed54af4006ee85c528a0c
dlemmermann <dlemmermann@994baef0-d455-0410-a6e6-f5fefbad0d02>
2011-12-02 11:06:01 +0000

    Fixed an issue where a cell renderer would get its background color set when selected instead of using the table selection color.

--- source/com/explodingpixels/macwidgets/plaf/ITunesTableUI.java
+++ source/com/explodingpixels/macwidgets/plaf/ITunesTableUI.java
@@ -28,13 +28,12 @@ import com.explodingpixels.widgets.TableUtils;
 import com.explodingpixels.widgets.WindowUtils;
 
 /**
- * A UI delegate that renders an iTunes style table.
- * <br/>
+ * A UI delegate that renders an iTunes style table. <br/>
  * <img src="../../../../../graphics/iTunesTable.png">
  * <p/>
  * Sorting indicators will be rendered if a
- * {@link com.explodingpixels.widgets.TableUtils.SortDelegate} is installed on the associated
- * {@link JTable} via the
+ * {@link com.explodingpixels.widgets.TableUtils.SortDelegate} is installed on
+ * the associated {@link JTable} via the
  * {@link TableUtils#makeSortable(javax.swing.JTable, com.explodingpixels.widgets.TableUtils.SortDelegate)}
  * method.
  */
@@ -43,11 +42,15 @@ public class ITunesTableUI extends BasicTableUI {
 	protected static final Color EVEN_ROW_COLOR = new Color(241, 245, 250);
 	protected static final Color TABLE_GRID_COLOR = new Color(0xd9d9d9);
 	protected static final Color SELECTION_ACTIVE_SELECTION_FOREGROUND_COLOR = Color.WHITE;
-	protected static final Color SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0x3d80df);
+	protected static final Color SELECTION_ACTIVE_SELECTION_BACKGROUND_COLOR = new Color(
+			0x3d80df);
 	protected static final Color SELECTION_INACTIVE_SELECTION_FOREGROUND_COLOR = Color.BLACK;
-	protected static final Color SELECTION_INACTIVE_SELECTION_BACKGROUND_COLOR = new Color(0xc0c0c0);
-	protected static final Color SELECTION_ACTIVE_BOTTOM_BORDER_COLOR = new Color(125, 170, 234);
-	protected static final Color SELECTION_INACTIVE_BOTTOM_BORDER_COLOR = new Color(224, 224, 224);
+	protected static final Color SELECTION_INACTIVE_SELECTION_BACKGROUND_COLOR = new Color(
+			0xc0c0c0);
+	protected static final Color SELECTION_ACTIVE_BOTTOM_BORDER_COLOR = new Color(
+			125, 170, 234);
+	protected static final Color SELECTION_INACTIVE_BOTTOM_BORDER_COLOR = new Color(
+			224, 224, 224);
 	protected static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
 
 	@Override
@@ -66,17 +69,20 @@ public class ITunesTableUI extends BasicTableUI {
 		table.setIntercellSpacing(new Dimension(0, 0));
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-		table.getTableHeader().setDefaultRenderer(new ITunesTableHeaderRenderer(table));
+		table.getTableHeader().setDefaultRenderer(
+				new ITunesTableHeaderRenderer(table));
 
 		table.setShowHorizontalLines(false);
 		TableHeaderUtils.makeHeaderFillEmptySpace(table);
 		TableUtils.makeStriped(table, EVEN_ROW_COLOR);
 
-		table.setDefaultRenderer(Rating.class, new ITunesRatingTableCellRenderer());
+		table.setDefaultRenderer(Rating.class,
+				new ITunesRatingTableCellRenderer());
 		table.setDefaultEditor(Object.class, createDefaultTableCellEditor());
 
 		makeTableActive();
-		WindowUtils.installWeakWindowFocusListener(table, createWindowFocusListener());
+		WindowUtils.installWeakWindowFocusListener(table,
+				createWindowFocusListener());
 	}
 
 	@Override
@@ -119,35 +125,41 @@ public class ITunesTableUI extends BasicTableUI {
 
 	public Border getSelectedRowBorder() {
 		return BorderFactory.createCompoundBorder(
-				BorderFactory.createMatteBorder(0, 0, 1, 0, getSelectedRowBottomHighlight()),
-				BorderFactory.createEmptyBorder(1, 5, 0, 5));
+				BorderFactory.createMatteBorder(0, 0, 1, 0,
+						getSelectedRowBottomHighlight()), BorderFactory
+						.createEmptyBorder(1, 5, 0, 5));
 	}
 
 	private Color getSelectedRowBottomHighlight() {
-		return WindowUtils.isParentWindowFocused(table)
-		? SELECTION_ACTIVE_BOTTOM_BORDER_COLOR : SELECTION_INACTIVE_BOTTOM_BORDER_COLOR;
+		return WindowUtils.isParentWindowFocused(table) ? SELECTION_ACTIVE_BOTTOM_BORDER_COLOR
+				: SELECTION_INACTIVE_BOTTOM_BORDER_COLOR;
 	}
 
 	/**
-	 * Creates a custom {@link CellRendererPane} that sets the renderer component to be non-opqaque
-	 * if the associated row isn't selected. This custom {@code CellRendererPane} is needed because
-	 * a table UI delegate has no prepare renderer like {@link JTable} has.
+	 * Creates a custom {@link CellRendererPane} that sets the renderer
+	 * component to be non-opqaque if the associated row isn't selected. This
+	 * custom {@code CellRendererPane} is needed because a table UI delegate has
+	 * no prepare renderer like {@link JTable} has.
 	 */
 	protected CellRendererPane createCustomCellRendererPane() {
 		return new CellRendererPane() {
 			@Override
-			public void paintComponent(Graphics graphics, Component component, Container container,
-					int x, int y, int w, int h, boolean shouldValidate) {
+			public void paintComponent(Graphics graphics, Component component,
+					Container container, int x, int y, int w, int h,
+					boolean shouldValidate) {
 
 				int rowAtPoint = table.rowAtPoint(new Point(x, y));
 				boolean isSelected = table.isRowSelected(rowAtPoint);
 				if (component instanceof JComponent) {
 					JComponent jComponent = (JComponent) component;
 					jComponent.setOpaque(isSelected);
-					jComponent.setBorder(isSelected ? getSelectedRowBorder() : getRowBorder());
-					jComponent.setBackground(isSelected ? jComponent.getBackground() : TRANSPARENT_COLOR);
+					jComponent.setBorder(isSelected ? getSelectedRowBorder()
+							: getRowBorder());
+					jComponent.setBackground(isSelected ? table
+							.getSelectionBackground() : TRANSPARENT_COLOR);
 				}
-				super.paintComponent(graphics, component, container, x, y, w, h, shouldValidate);
+				super.paintComponent(graphics, component, container, x, y, w,
+						h, shouldValidate);
 			}
 		};
 	}
